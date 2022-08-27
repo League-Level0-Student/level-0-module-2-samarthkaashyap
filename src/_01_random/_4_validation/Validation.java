6f4fc21a1582029949b00054ec2a8ee1564f45f8
league <league@imac-14.attlocal.net>
2022-08-27 16:24:57 -0700

    diceroll, intro to radnom, rockpaperscissors, validation

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -11,10 +11,30 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
+		for (int x = 0; x < 10; x+=1) {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
+		if (randomNumber == 1) {
+			System.out.print("You are looking good today.");
+		}
+		if (randomNumber == 2) {
+			System.out.print("You are smart.");
+		}
+		if (randomNumber == 3) {
+			System.out.print("You are good at video games.");
+		}
+		if (randomNumber == 4) {
+			System.out.print("You are awesome.");
+		}
+		if (randomNumber == 0) {
+			System.out.print("You are good at programming.");
+		}
+		}
+
+
+
+
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 
